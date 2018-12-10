4c9cdb7ea35035aab687f0f645f572eccc8df73a
iabramova <34316291+iabramova@users.noreply.github.com>
2018-12-10 11:05:00 -0800

    Add files via upload

--- MathematicaPackages/PackageDev/TFStep.m
+++ MathematicaPackages/PackageDev/TFStep.m
@@ -19,7 +19,7 @@ BeginPackage["PackageDev`TFStep`",{"PackageDev`TFCheck`","PackageDev`TFRoot`"}];
 
 (* ::Input::Initialization:: *)
 (*Create a function to format the given transfer function in the Matlab standard convention*)
-TFStep`tfStepBuild[userStepNumerator_,userStepDenominator_,steprange1_,steprange2_]:= Module[{tfStep,tfStepModel,stepPlotHandle,rootAdjustedData,sTFStepBuild,tTFStepBuild,stepOutputResponse},sTFStepBuild = Symbol["s"];tTFStepBuild = Symbol["t"];tfStep = TransferFunctionModel[(TFCheck`tf[TFCheck`tfBuild[userStepNumerator,userStepDenominator]]),sTFStepBuild];stepOutputResponse = OutputResponse[tfStep,UnitStep[tTFStepBuild],tTFStepBuild];stepPlotHandle = Plot[stepOutputResponse,{tT,steprange1,steprange2}//.tT-> tTFStepBuild];Flatten[TFRoot`rootDataExtract[stepPlotHandle],1]]
+TFStep`tfStepBuild[userStepNumerator_,userStepDenominator_,steprange1_,steprange2_]:= Module[{tfStep,tfStepModel,stepPlotHandle,rootAdjustedData,sTFStepBuild,tTFStepBuild,stepOutputResponse},sTFStepBuild = Symbol["s"];tTFStepBuild = Symbol["t"];tfStep = TransferFunctionModel[(TFCheck`tf[TFCheck`tfBuild[userStepNumerator,userStepDenominator]]),sTFStepBuild];stepOutputResponse = OutputResponse[tfStep,UnitStep[tTFStepBuild],tTFStepBuild];stepPlotHandle = Plot[stepOutputResponse,{tT,steprange1,steprange2}//.tT-> tTFStepBuild,PlotRange->All];Flatten[TFRoot`rootDataExtract[stepPlotHandle],1]]
 
 
 (* ::Input::Initialization:: *)
